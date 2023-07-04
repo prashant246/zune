d92b2b709f51caed2c11129e0a099426efcfc9fa
Prashant Gupta <prashantgupta@Prashants-MacBook-Pro.local>
2023-07-04 13:47:34 +0530

    initial-commit

---
+++ src/main/java/com/aggregator/zune/ZuneApplication.java
@@ -0,0 +1,13 @@
+package com.aggregator.zune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aggregator/zune/ZuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aggregator.zune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
